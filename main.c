43997de770efe5d5b76b6ceae6ca1022990bd87a
zibbe77 <theo@zibll.se>
2024-01-19 09:56:27 +0100

    addade mutex

--- main.c
+++ main.c
@@ -3,17 +3,22 @@
 #include <unistd.h>
 
 int num = 0;
+pthread_mutex_t mutex;
 
 void *threedTest()
 {
-    for (int i = 0; i < 100; i++)
+    for (int i = 0; i < 2000000; i++)
     {
+        pthread_mutex_lock(&mutex);
         num++;
+        pthread_mutex_unlock(&mutex);
     }
 }
 
 int main()
 {
+    pthread_mutex_init(&mutex, NULL);
+
     pthread_t t1;
     pthread_t t2;
     pthread_create(&t1, NULL, &threedTest, NULL);
@@ -21,6 +26,8 @@ int main()
 
     pthread_join(t2, NULL);
     pthread_join(t1, NULL);
+
+    pthread_mutex_destroy(&mutex);
 
     printf("test %d", num);
 
